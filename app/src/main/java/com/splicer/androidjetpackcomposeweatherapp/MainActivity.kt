3190c301cafa0de9bff428d2c0971456dfaddb01
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-24 21:55:17 +0300

    lesson_12

--- app/src/main/java/com/splicer/androidjetpackcomposeweatherapp/MainActivity.kt
+++ app/src/main/java/com/splicer/androidjetpackcomposeweatherapp/MainActivity.kt
@@ -1,29 +1,17 @@
 package com.splicer.androidjetpackcomposeweatherapp
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.splicer.androidjetpackcomposeweatherapp.screens.MainScreen
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.splicer.androidjetpackcomposeweatherapp.screens.MainCard
+import com.splicer.androidjetpackcomposeweatherapp.screens.TabLayout
 import com.splicer.androidjetpackcomposeweatherapp.ui.theme.AndroidJetpackComposeWeatherAppTheme
-import org.json.JSONObject
 
 const val API_KEY = "b0676713ebfe49efbf8174320221907 "
 
@@ -32,75 +20,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidJetpackComposeWeatherAppTheme {
-                MainScreen()
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("London", this)
-//                }
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.weather_bg
+                    ),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, context: Context) {
-    val state = remember {
-        mutableStateOf("Unknown")
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Temperature in $name: ${state.value} Cº")
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Button(
-                onClick = { getData(name, state, context) }, modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Refresh")
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidJetpackComposeWeatherAppTheme {
-
-    }
-}
-
-fun getData(city: String, state: MutableState<String>, context: Context) {
-    val url = "https://api.weatherapi.com/v1/current.json" +
-            "?key=$API_KEY&" +
-            "q=$city" +
-            "&aqi=no"
-    val queue = Volley.newRequestQueue(context)
-    val stringRequest = StringRequest(
-        Request.Method.GET,
-        url,
-        { response ->
-            val obj = JSONObject(response)
-            val temp = obj.getJSONObject("current")
-            state.value = temp.getString("temp_c")
-            Log.d("MyLog", "Response: ${temp.getString("temp_c")}")
-        },
-        {
-            Log.d("MyLog", "Volley error: $it")
-        }
-    )
-    queue.add(stringRequest)
 }
