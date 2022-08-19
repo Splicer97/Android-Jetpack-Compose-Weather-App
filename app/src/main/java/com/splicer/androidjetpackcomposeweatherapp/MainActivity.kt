6cc1842a63611fbab90e751163ed3aee167fd043
Nikita_Kovantsov <kovancov@gmail.com>
2022-08-19 23:09:47 +0300

    lesson_15

--- app/src/main/java/com/splicer/androidjetpackcomposeweatherapp/MainActivity.kt
+++ app/src/main/java/com/splicer/androidjetpackcomposeweatherapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.splicer.androidjetpackcomposeweatherapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,6 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.splicer.androidjetpackcomposeweatherapp.screens.MainCard
 import com.splicer.androidjetpackcomposeweatherapp.screens.TabLayout
 import com.splicer.androidjetpackcomposeweatherapp.ui.theme.AndroidJetpackComposeWeatherAppTheme
@@ -20,6 +25,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidJetpackComposeWeatherAppTheme {
+                getData("London", this)
                 Image(
                     painter = painterResource(
                         id = R.drawable.weather_bg
@@ -37,4 +43,25 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun getData(city: String, context: Context){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+                response ->
+            Log.d("MyLog", "Response: $response")
+        },
+        {
+            Log.d("MyLog", "VolleyError: $it")
+        }
+    )
+    queue.add(sRequest)
 }
